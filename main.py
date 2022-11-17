e91cbe0dfa76d500d87a92cda437881c61c05647
nolansorich <nolan.sorich@gmail.com>
2022-11-17 12:15:04 -0800

    added core files

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+my final project is flying bird
+'''
+
+'''
+sources:
+i used ____ to learn about ____
+'''
+
+# import libraries 
+import pygame as pg
+
+# built in libraries
+import tkinter as tk
+
+# created libraries
+import settings
+import sprites
